4dc28dac51247be880f340765935a2feadc65770
ivanstepanov <ivanstepanoff@yahoo.com>
2020-08-27 13:25:54 +0300

    C:/Program Files/Git/debet_card is modified and /meow is added

---
+++ meow/precache-manifest.e18c8d592dfe1b9f943255e1d60ed80b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "881dc4f1112b2a966b1d875e09c6ff08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a1e379ec028cce4ee59",
+    "url": "/static/css/main.2def2e2d.chunk.css"
+  },
+  {
+    "revision": "3a05da1d68ff7fb0b364",
+    "url": "/static/js/2.ad04c07d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ad04c07d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a1e379ec028cce4ee59",
+    "url": "/static/js/main.9083de9d.chunk.js"
+  },
+  {
+    "revision": "5cd739c604a0b9014e8c",
+    "url": "/static/js/runtime-main.c6df02e2.js"
+  },
+  {
+    "revision": "cf34c858a5410187c4754b3127596f76",
+    "url": "/static/media/bg.cf34c858.png"
+  },
+  {
+    "revision": "2fbc26393277c4a301e983ef4dcf2a8c",
+    "url": "/static/media/exo2.0-thin-webfont.2fbc2639.woff"
+  },
+  {
+    "revision": "a0e8088599fbeb52e05b1f8bfaa75be9",
+    "url": "/static/media/exo2.0-thin-webfont.a0e80885.woff2"
+  },
+  {
+    "revision": "1cc3a5db5641af1281c5a88ebaa72614",
+    "url": "/static/media/kote.1cc3a5db.png"
+  }
+]);
